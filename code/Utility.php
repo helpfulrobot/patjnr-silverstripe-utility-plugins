c0d189f8eb25f6dd675466c4545e1e97aa0906ad
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:52:31 +1300

    Converted to PSR-2

--- code/Utility.php
+++ code/Utility.php
@@ -6,17 +6,15 @@
 class Utility extends Object
 {
 
-    static function include_code()
+    public static function include_code()
     {
         self::includePlugins();
         self::includeMasonry();
 
         Requirements::javascript(UTILITY_DIR . '/js/smartresize.js');
-
-
     }
 
-    static function includeBootstrap($jquery = false)
+    public static function includeBootstrap($jquery = false)
     {
         if ($jquery) {
             self::includeJquery();
@@ -25,19 +23,19 @@ class Utility extends Object
         Requirements::javascript(BOOTSTRAP_DIR . '/js/bootstrap.js');
     }
 
-    static function includeMasonry()
+    public static function includeMasonry()
     {
         Requirements::javascript(MASONRY_DIR . '/dist/masonry.pkgd.min.js');
     }
 
-    static function includeChosen()
+    public static function includeChosen()
     {
         Requirements::javascript(CHOSEN_DIR . '/chosen.jquery.min.js');
         Requirements::javascript(PLUGINS_DIR . '/chosen/js/chosen.jquery_init.js');
         Requirements::css(CHOSEN_DIR . '/chosen.css');
     }
 
-    static function includeJquery()
+    public static function includeJquery()
     {
         Requirements::block(THIRDPARTY_DIR . "/jquery/jquery.js");
         Requirements::javascript(JQUERY_DIR . '/jquery.js');
@@ -57,7 +55,6 @@ class Utility extends Object
         Requirements::javascript(UNDERSCORE_DIR . '/underscore.js');
         Requirements::javascript(MOMENT_DIR . '/moment.js');
         Requirements::css(UTILITY_BOWER_DIR . '/components-font-awesome/css/font-awesome.min.css');
-
     }
 
     public static function includeIsotope()
